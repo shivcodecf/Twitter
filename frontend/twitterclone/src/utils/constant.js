5e4d0d4e4f1759c392dfab63c54d6b2e23698a04
shivcodecf <shivam2113128@akgec.ac.in>
2025-01-25 23:16:30 +0530

    backend url

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "https://twitter-back-evfw.onrender.com/api/v1/user";
-export const TWEET_API_END_POINT = "https://twitter-back-evfw.onrender.com/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-back1.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-back1.onrender.com/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     if (!timestamp) return "Invalid date"; // Handle missing timestamp
