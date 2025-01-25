7974e50f0c3244a63845e646c3824c4a658876a2
shivcodecf <shivam2113128@akgec.ac.in>
2025-01-25 22:42:22 +0530

    frontend url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:3000",
+    origin:"https://twitter-frontened.vercel.app/login",
     credentials:true
 }
 app.use(cors(corsOptions));
